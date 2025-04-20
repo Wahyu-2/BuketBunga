9d55920b16664759fc54bcbc604e2b5c1f9b6b31
Wahyu-2 <wahyuperwirasanusi0323@gmail.com>
2025-04-20 16:54:02 +0700

    INI SAMPE YANG B TINGGAL YANG C

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,20 @@
-<div>
-    <!-- Very little is needed to make a happy life. - Marcus Aurelius -->
-    Selamat Datang di home {{$nama}}<br>
-    Anda Adalah Seorang {{$pekerjaan}}
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Contoh View</title>
+  <!-- Langkah 5: Tambahkan link rel ke file CSS -->
+  <link rel="stylesheet" href="{{ asset('/styles026/styles026.css') }}">
+</head>
+<body>
+
+  <!-- Langkah 4: Tambahkan tag <h1> -->
+  <h1>Halo Dunia!</h1>
+
+  <!-- Langkah 3: Tambahkan 2 gambar -->
+  <img src="{{ asset('image_026/flower1.jpg') }}" alt="Gambar 1" width="300px">
+  <img src="{{ asset('image_026/flower2.jpg') }}" alt="Gambar 2" width="300px">
+  <img src="{{ asset('image_026/flower3.jpg') }}" alt="Gambar " width="300px">
+  
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,6 @@
 use Illuminate\Support\Facades\Route;
 Use App\Http\Controllers\HomeController;
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
-
-Route::get('/', [HomeController::class, 'index']);
-Route::get('/contact', [HomeController::class, 'contact']);
+Route::get('/', function () {
+    return view('home');
+});
